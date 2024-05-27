6a119f0747175b5ab3c864b4e440cc72d2aa226a
Konstantin Dyachenko <drakonard@gmail.com>
2024-05-27 21:03:15 +0000

    Add first question with three answer options

--- index.js
+++ index.js
@@ -7,12 +7,43 @@ const token = fs.readFileSync('token', 'utf8').trim();
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches any message
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+const welcomeMessage = `Здравствуйте. Я бот Высшей школы стилистики, дизайна и технологий! Я готов предоставить вам информацию о наших курсах и вариантах обучения. Также я могу перевести вас на другого бота, где вы можете бесплатно проходить обучение.`;
 
-    // Send back 'Hello, World!' to the sender
-    bot.sendMessage(chatId, 'Hello, World!');
+// // Matches any message
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+
+//     // Send back 'Hello, World!' to the sender
+//     bot.sendMessage(chatId, welcomeMessage);
+// });
+
+// Handle callback queries
+bot.on('callback_query', (callbackQuery) => {
+  const message = callbackQuery.message;
+  const data = callbackQuery.data;
+
+  bot.sendMessage(message.chat.id, `You selected option ${data}`);
+});
+
+// Matches /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  // Send back 'Hello, World!' to the sender
+  bot.sendMessage(chatId, welcomeMessage).then(() => {
+
+    // Send follow-up question with options
+    bot.sendMessage(chatId, 'Как я могу быть вам полезен?', {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Информация о курсах', callback_data: '1' }],
+          [{ text: 'Оставить заявку для связи', callback_data: '2' }],
+          [{ text: 'Хочу учиться бесплатно!', callback_data: '3' }]
+        ]
+      }
+    });
+
+  });;
 });
 
 console.log("Bot is running...");
